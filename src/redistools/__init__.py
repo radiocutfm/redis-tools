663e5147425ba92770f40b72ad1e927229e1dfbf
Guillermo M. Narvaja <guillermo.narvaja@radiocut.fm>
2021-02-25 19:48:28 -0300

    db parameter for sentinel connections.

--- src/redistools/__init__.py
+++ src/redistools/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 import json
 import functools
@@ -79,8 +79,8 @@ def get_redis(master=True, reconnect=False, env=None):
             "REDIS_TIMEOUT", 0.2
         )
         sentinel = MySentinel([(host, port)], socket_timeout=REDIS_TIMEOUT)
-        _redis_master = sentinel.master_for(REDIS_MASTER, socket_timeout=REDIS_TIMEOUT)
-        _redis_slave = sentinel.slave_for(REDIS_MASTER, socket_timeout=REDIS_TIMEOUT)
+        _redis_master = sentinel.master_for(REDIS_MASTER, socket_timeout=REDIS_TIMEOUT, db=db)
+        _redis_slave = sentinel.slave_for(REDIS_MASTER, socket_timeout=REDIS_TIMEOUT, db=db)
 
     return _redis_master if master else _redis_slave
 
